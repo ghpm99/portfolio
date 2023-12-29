7c7f2a75920f668da2cdd56833d4ac7617cbb0da
Guilherme H <ghpm99@gmail.com>
2023-12-28 23:39:13 -0300

    Feat: Implements update at

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -11,6 +11,8 @@ interface IProject {
 	homepage: string;
 	html_url: string;
 	topics: string[];
+	language: string;
+	pushed_at: string;
 }
 
 const Projects = ({log}: {log: (data: string) => void}) => {
@@ -28,6 +30,11 @@ const Projects = ({log}: {log: (data: string) => void}) => {
 		loadProjectsData();
 	}, []);
 
+	const formatData = (data: string) => {
+		const date = new Date(data);
+		return date.toLocaleDateString();
+	}
+
 	return (
 		<div>
 			<div id='projects'>Projetos</div>
@@ -35,7 +42,7 @@ const Projects = ({log}: {log: (data: string) => void}) => {
 				{projects.map((project) => (
 					<div
 						key={project.id}
-						className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 cursor-pointer m-4 w-[300px] h-[600px]'
+						className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 m-4 w-[300px] h-[600px]'
 					>
 						<Link
 							href={project.html_url}
@@ -64,6 +71,12 @@ const Projects = ({log}: {log: (data: string) => void}) => {
 									{topic}
 								</span>
 							))}
+						</div>
+						<div>
+							<div>
+							{project.language}
+							</div>
+							<span>Ultima atualização: {formatData(project.pushed_at)}</span>
 						</div>
 					</div>
 				))}
